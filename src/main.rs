208a8f4ac7002c61bc4f2db6ca00e0eab06834c2
Blake Wyatt <nabakin@protonmail.ch>
2020-09-12 19:01:03 -0400

    Init with example ping pong code

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+use tokio::net::TcpListener;
+use tokio::prelude::*;
+use serenity::async_trait;
+use serenity::client::{Client, Context, EventHandler};
+use serenity::model::channel::Message;
+use serenity::framework::standard::{
+    StandardFramework,
+    CommandResult,
+    macros::{
+        command,
+        group
+    }
+};
+
+mod config;
+
+use std::env;
+
+#[group]
+#[commands(ping)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {}
+
+#[tokio::main]
+async fn main() {
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("!")) // set the bot's prefix to "~"
+        .group(&GENERAL_GROUP);
+
+    let mut client = Client::new(config::token)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!").await?;
+
+    Ok(())
+}
